1a107880771345a55123b0c6b8f0722a437cd94c
adityaoryza <oryza429@gmail.com>
2023-04-27 17:09:58 +0700

    add API documentation using swager

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const apicache = require("apicache");
 // routes v1 workouts
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+// API documentation
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const cache = apicache.middleware;
@@ -17,4 +19,6 @@ app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+  // API documentation
+  V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -5,6 +5,56 @@ const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 const router = express.Router();
 const cache = apicache.middleware;
+// middlware to check for security issues
+// const authenticate = require("../../middlewares/authenticate");
+// const authorize = require("../../middlewares/authorize");
+
+// to documentation this API using openAPI documentation
+// NOTE : future task => document all the routes on the swagger
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
 
 // caching to get faster performance
 router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
@@ -15,6 +65,9 @@ router.get("/:workoutId", workoutController.getOneWorkout);
 
 // record routes
 router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
+// example of security check using middleware
+// router.post("/", authenticate, authorize, workoutController.createNewWorkout);
 
 router.post("/", workoutController.createNewWorkout);
 
